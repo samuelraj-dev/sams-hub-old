42265f50c55e1a4277bf7b16369ba0aa9a7de0e6
samuelraj-dev <samuelraj.dev@outlook.com>
2022-05-28 12:31:35 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import './App.css'
+
+const BACKEND = process.env.BACKEND || 'http://localhost:5000/'
+
+const App = () => {
+
+    const [name, setName] = useState()
+    const [age, setAge] = useState(0)
+    const [friendList, setFriendList] = useState([])
+
+    const addFriend = () => {
+        axios.post(`${BACKEND}addfriend`, {
+            name: name,
+            age: age,
+        })
+            .then((res) => {
+                setFriendList([...friendList, {_id: res.data._id, name: name, age: age,}])
+            })
+            .catch((err) => console.log(err))
+    }
+
+    const updateFriend = (id) => {
+        const newAge = prompt('Enter new age: ')
+
+        axios.put(`${BACKEND}updatefriend`, { id: id, newAge: newAge})
+            .then(() => {
+                setFriendList(friendList.map((data) => {
+                    if (data._id === id) {
+                        return {_id: id, name: data.name, age: newAge}
+                    }
+                    else {
+                        return data
+                    }
+                }))
+            })
+    }
+
+    const deleteFriend = (id) => {
+        axios.delete(`${BACKEND}deletefriend/${id}`)
+            .then(() => {
+                setFriendList(friendList.filter((data) => {
+                    return data._id !== id
+                }))
+            })
+    }
+
+    useEffect(() => {
+        axios(`${BACKEND}readfriend`)
+            .then((data) => {
+                setFriendList(data.data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
+
+    return (
+        <div className="App">
+            <input type="text" placeholder='Friend name' onChange={(e) => {setName(e.target.value)}} />
+            <input type="number" placeholder='Friend age' onChange={(e) => {setAge(e.target.value)}} />
+            <input type="button" value="Add Friend" onClick={addFriend} />
+            <div>
+                {
+                    friendList.map((data) => {
+                        return (
+                            <>
+                                <div>{data.name} {data.age}</div>
+                                <button onClick={() => updateFriend(data._id)}>Update</button>
+                                <button onClick={() => deleteFriend(data._id)}>Delete</button>
+                            </>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default App
